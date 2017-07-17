ca1a95b7f9b3c8bceaf7b69735d7a40e89ef06dd
gunjandeora <gunjan.deora@metacube.com>
2017-07-17 18:00:32 +0530

    Update A1_Test.java

--- Session2/A1_Test.java
+++ Session2/A1_Test.java
@@ -1,17 +1,13 @@
 package PF_Session2;
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 public class A1_Test {
-
 	@Test
 	public void test() {
 		DrawPattern1 drawp = new DrawPattern1();
 		String expected1 = "   12321";					//row = 9 and key = 6
 		String expected2 = "1234321";					//row = 3 and key = 7 
-		String expected3 = "   ";						//row = 8 and key = 5 
+		String expected3 = "   ";					//row = 8 and key = 5 
 		String fexpected1 = " ";
 		String fexpected2 = " ";
 		String fexpected3 = " ";
@@ -22,7 +18,5 @@ public class A1_Test {
 		assertEquals(fexpected1, drawp.MergeStrings(16, 6));
 		assertEquals(fexpected2, drawp.number(-5, 7));
 		assertEquals(fexpected3, drawp.space(21, 7));
-		
 	}
-
 }
