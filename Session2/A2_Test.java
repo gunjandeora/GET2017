f57be381acfa95fb50ea0fb8e5941d7da9de4d2f
gunjandeora <gunjan.deora@metacube.com>
2017-07-17 18:01:11 +0530

    Update A2_Test.java

--- Session2/A2_Test.java
+++ Session2/A2_Test.java
@@ -1,14 +1,9 @@
 package PF_Session2;
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 public class A2_Test {
-
 	@Test
-	public void space() {
-		
+	public void space() {		
 		DrawPattern2 drawp = new DrawPattern2();
 		String expected1 = "   123";					//key = 6 and row = 3
 		String expected2 = "1234567";					//key = 7 and row = 0
@@ -22,8 +17,6 @@ public class A2_Test {
 		assertEquals(expected3, drawp.space(5, 7));
 		assertEquals(fexpected1, drawp.MergeStrings(-3, -6));		//false input , when both negative values
 		assertEquals(fexpected2, drawp.number(-12, 7));				//false input , when both negative values
-		assertEquals(fexpected3, drawp.space(14, 7));
-		
+		assertEquals(fexpected3, drawp.space(14, 7));	
 	}
-
 }
