0dc77ed754f86be2c96560e9be1537889c2f7409
Gunjan <gunjandeora@gmail.com>
2017-08-23 14:00:32 +0530

    Data Structure Assignment2 Stack

---
+++ Stack/Node.java
@@ -0,0 +1,10 @@
+package ds2.Stack;
+public class Node {
+	public Character value;
+	public Node next;
+	public Node previous;
+   	public Node(Character item){
+            value = item;	
+	}
+        public Node(){}
+}
