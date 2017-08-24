f951406fb8a09eec49505ff6154872bd219af28e
gunjandeora <gunjan.deora@metacube.com>
2017-08-24 16:17:57 +0530

    Delete DesignPattern1.java

--- designpattern1/DesignPattern1.java
+++
@@ -1,9 +0,0 @@
-package designpattern1;
-public class DesignPattern1 {
-    public static void main(String[] args) {
-        Menu menu=Menu.createMenu();
-	while(true){
-	Menu.displayAndPerformAction(menu);
-	}	
-    }
-}
