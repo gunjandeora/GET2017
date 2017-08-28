62e9b1088a3649ea3fdaa1336c3a2fd3c7594100
Gunjan <gunjandeora@gmail.com>
2017-08-28 14:13:37 +0530

    DS4

---
+++ DS4/RoomAllocation/MyHashTable.java
@@ -0,0 +1,38 @@
+package DS4.RoomAllocation;
+public class MyHashTable {
+	Room roomArr[];
+	int size;
+	MyHashTable(int size, Room room[]) {
+		this.size = size;
+		roomArr = room;
+	}
+	//Calculating hash key
+	int hashFunction(int key) {
+		return key % size;
+	}
+	void allocateRoom(Guest guest) {
+		int index = hashFunction(guest.getAge());
+		int counter = 0;
+		while (roomArr[index].getGuest() != null) {
+			index++;
+			System.out.println("index" + index);
+			index = index % size;
+			counter++;
+			if (counter == size) {
+				System.out.println("hi guest" + counter);
+				System.out.println("No Room Available for you :( ");
+				return;
+			}
+		}
+		roomArr[index].setGuest(guest);
+	}
+	void printDetail() {
+		System.out.println("Room No\t  Guest Name\tGuest Age");
+		for (Room room : roomArr) {
+			if (room.getGuest() != null)
+				System.out.println(room.getRoomNo() + "\t  "
+						+ room.getGuest().getName() + "\t\t"
+						+ room.getGuest().getAge());
+		}
+	}
+}
---
+++ DS4/RoomAllocation/Room.java
@@ -0,0 +1,18 @@
+package DS4.RoomAllocation;
+public class Room {
+	int roomNo;
+	Guest guest;
+	Room(int roomNo) {
+		this.roomNo = roomNo;
+		guest = null;
+	}
+	int getRoomNo() {
+		return this.roomNo;
+	}
+	void setGuest(Guest guest) {
+		this.guest = guest;
+	}
+	Guest getGuest() {
+		return this.guest;
+	}
+}
