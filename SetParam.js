34209623f09d47f2a76cb44e8b74611a57ee2b25
gunjandeora <gunjan.deora@metacube.com>
2017-08-28 11:19:20 +0530

    Add files via upload

---
+++ SetParam.js
@@ -0,0 +1,14 @@
+function setPara(){
+	var contact = document.getElementById("contact_display"); //Get span
+	a_contact = document.createElement('a');
+	a_contact.href =  'ContactUs.html'; 
+	a_contact.textContent = 'Contact Us'; // <a>INNER_TEXT</a>
+	contact.appendChild(a_contact); // Append the link to the span
+	
+	var home = document.getElementById("home_display");
+	a_home = document.createElement('a');
+	a_home.href =  'index.html'; 
+	a_home.textContent = 'Home'; // <a>INNER_TEXT</a>
+	home.appendChild(a_home); // Append the link to the span
+
+}
