01cc8643d59af026477a2424b64d90cca23bcebb
gunjandeora <gunjan.deora@metacube.com>
2017-08-29 17:59:07 +0530

    Update UserDao.java

--- ModifiedFiles/UserDao.java
+++ ModifiedFiles/UserDao.java
@@ -6,8 +6,8 @@ import org.json.simple.JSONObject;
 public interface UserDao {
 	boolean deleteEmployee(int id);
 	JSONObject getEmployeeDetails(int id);
-	boolean addEmployee(JSONObject employee);
-	boolean updateEmployee(int id);
+	JSONObject addEmployee(JSONObject employee);
+	JSONArray updateEmployee(int id);
 	JSONArray getAllEmployees();
 	JSONArray getAllEmployeesByName(String name);
 }
