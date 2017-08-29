401f9aa198bb47a096fe62ed0657722ae4302c3b
gunjandeora <gunjan.deora@metacube.com>
2017-08-30 02:05:25 +0530

    Update Employee.java

--- Employee/Employee.java
+++ Employee/Employee.java
@@ -1,42 +1,44 @@
 package DS6.NewEmployee;
-public class Employee implements Comparable<Object> {
-	private int employeeId;
-	private String name;
-	private String address;
-	public Employee(int employeeId, String name, String address) {
-		this.employeeId = employeeId;
-		this.name = name;
-		this.address = address;
+public class Employee implements Comparable<Object>{
+    private String employeeId;
+    private String name;
+    private String address;
+    public Employee(String employeeId, String name, String address) {
+            this.employeeId = employeeId;
+            this.name = name;
+            this.address = address;
 	}
-	public void setEmployeeId(int employeeId){
-		this.employeeId = employeeId;
+	public void setEmployeeId(String employeeId){
+            this.employeeId = employeeId;
 	}
-	public int getEmployeeId() {
-		return this.employeeId;
+	public String getEmployeeId() {
+            return this.employeeId;
 	}
 	public void setName(String name){
-		this.name = name;
+            this.name = name;
 	}
 	public String getName() {
-		return this.name;
+            return this.name;
 	}
 	public void setAddress(String address){
-		this.address = address;
+            this.address = address;
 	}
 	public String getAddress() {
-		return this.address;
+            return this.address;
 	}
 	/*
 	 * Overriding compareTo method and 
 	 * Comparing according to employee IDs(Natural Order)
-	 */
-	@Override
-	public int compareTo(Object o) {
-		return ((Integer)this.getEmployeeId()).compareTo((Integer)((Employee) o).getEmployeeId());
-	}
-	public String toString(){
-		String str="";	
-		str+= "emp id="+this.emp_ID+" name= "+this.name+" address= "+this.address+"\n";
-		return str;
-	}
+	 */   
+    @Override
+    public int compareTo(Object o) {
+       //return getEmployeeId().compareTo(o.getEmployeeId());
+       return getEmployeeId().compareTo( ((Employee)o).getEmployeeId() );
+    }
+    
+    public String toString(){
+        String str="";	
+        str+= "emp id="+this.employeeId+" name= "+this.name+" address= "+this.address+"\n";
+        return str;
+    }
 }
