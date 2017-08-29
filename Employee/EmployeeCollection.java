e2100cf976225b2fc70765cab950827c87a8bdf5
gunjandeora <gunjan.deora@metacube.com>
2017-08-30 03:17:19 +0530

    Update EmployeeCollection.java

--- Employee/EmployeeCollection.java
+++ Employee/EmployeeCollection.java
@@ -1,56 +1,58 @@
 package DS6.NewEmployee;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import DS2.CounsellingSystem.*;
-/*
- * Class to perform operation on Employee Collection.
- */
-public class EmployeeCollection {
-	MyArrayList employees;
-	/**
-	 * Constructor
-	 */
-	public EmployeeCollection(){
-		employees = new MyArrayList();
-	}
-	/*
-	 * Performs the Natural Sorting on the collection
-	 * of employees
-	 */
-	public void sort() {
-		employees.sort();
-	}
-	/*
-	 * Sort the employee collection on the basis of their names
-	 */
-	public void sortByName() {
-		employees.sort(employees, new Comparator<Object>() {
-            @Override
-            public int compare(Object first, Object second) {
-               Employee employeeOne = (Employee) first;
-               Employee employeeTwo = (Employee) second;
-               return(employeeOne.getName().compareTo(employeeTwo.getName()));
-            }
-        });
-	}
-	/*
-	 * returns the unique collection of employees
-	 * on the basis of their ids
-	 */
-	public Set<Employee> getUniqueEmployees(){
-		Map<Integer,Employee> uniqueEmployeeMapList = new HashMap<Integer,Employee>();
-		for(Employee employee: employees) {
-			uniqueEmployeeMapList.put(employee.getEmpID(), employee);
-		}
-		for(int emp=0;emp<employees.size();emp++){
-			uniqueEmployeeMapList.put(employees[emp].getEmpID(), employees[emp]);
-		}
-		Set<Employee> uniqueAccs = new HashSet<Employee>(uniqueEmployeeMapList.values());    
-		return uniqueAccs;
-	}
+public class Employee implements Comparable<Object>{
+    private String employeeId;
+    private String name;
+    private String address;
+    public Employee(String employeeId, String name, String address) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.address = address;
+    }
+    public void setEmployeeId(String employeeId){
+        this.employeeId = employeeId;
+    }
+    public String getEmployeeId() {
+        return this.employeeId;
+    }
+    public void setEmployeeName(String name){
+        this.name = name;
+    }
+    public String getEmployeeName() {
+        return this.name;
+    }
+    public void setEmployeeAddress(String address){
+        this.address = address;
+    }
+    public String getEmployeeAddress() {
+        return this.address;
+    }
+    /*
+     * Overriding compareTo method and 
+     * Comparing according to employee IDs(Natural Order)
+     */   
+    @Override
+    public int compareTo(Object o) {
+       //return getEmployeeId().compareTo(o.getEmployeeId());
+       return getEmployeeId().compareTo( ((Employee)o).getEmployeeId() );
+    }
+    public String toString(){
+        String str="";	
+        str+= "emp id="+this.employeeId+" name= "+this.name+" address= "+this.address+"\n";
+        return str;
+    }
+}
+// Comparator for sorting according to Name
+class SortByName implements Comparator<Object>{
+    @Override
+    public int compare(Object emp1, Object emp2) {
+       return ( ((Employee)emp1).getEmployeeName() ).compareTo( ((Employee)emp2).getEmployeeName() );
+    }
+}
+// Comparator for sorting according to Name
+class SortByAddress implements Comparator<Object>{
+    @Override
+    public int compare(Object emp1, Object emp2) {
+       return ( ((Employee)emp1).getEmployeeAddress() ).compareTo( ((Employee)emp2).getEmployeeAddress() );
+    }
 }
