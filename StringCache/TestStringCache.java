3a08d4dcaff1250d0eead6feac8a070fb1cc51b4
gunjandeora <gunjan.deora@metacube.com>
2017-08-30 15:00:37 +0530

    Update TestStringCache.java

--- StringCache/TestStringCache.java
+++ StringCache/TestStringCache.java
@@ -8,23 +8,22 @@ import org.junit.Test;
 public class TestStringCache {
 	StringCache sCache = new StringCache();
 	String input1 = "hello",
-			input2 = "world!",
-			input3 = "hello world!",
-			input4 = "hello";
+		input2 = "world!",
+		input3 = "hello world!",
+		input4 = "hello";
 	Integer expected1 = 4,
-			expected2 = 6,	
-			expected3 = 9;
+		expected2 = 6,	
+		expected3 = 9;
 	Map<String, Integer> expectedMap = new LinkedHashMap<String, Integer>();
 	@Test
 	public void test() {
 		expectedMap.put("hello", 4);
 		assertEquals(expected1, sCache.checkOccurrence(input1));
-		// re-using map and set
 		expectedMap.put("world!", 6);
 		assertEquals(expected2, sCache.checkOccurrence(input2));
 		expectedMap.put("hello world!", 9);
 		assertEquals(expected3, sCache.checkOccurrence(input3));
-		assertEquals(expected1, sCache.checkOccurrence(input4));
+		assertEquals(expected1, sCache.checkOccurrence(input4));			//returns saved value for specific key
 		System.out.println(sCache.cacheMap);
 	}
 }
