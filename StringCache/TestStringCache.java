7e3ccc27735f3bcecb91dbfe9a45936c2e05fe90
gunjandeora <gunjan.deora@metacube.com>
2017-08-30 14:36:57 +0530

    Update TestStringCache.java

--- StringCache/TestStringCache.java
+++ StringCache/TestStringCache.java
@@ -1,18 +1,19 @@
 package DS6.StringCache;
+package DS6.StringCache;
 import static org.junit.Assert.*;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 import org.junit.Test;
-
+// test class for string cache
 public class TestStringCache {
 	StringCache sCache = new StringCache();
 	String input1 = "hello",
-		input2 = "hello world";
+			input2 = "world!",
+			input3 = "hello world!",
+			input4 = "hello";
 	Integer expected1 = 4,
-			expected2 = 9;	
+			expected2 = 6,	
+			expected3 = 9;
 	Map<String, Integer> expectedMap = new LinkedHashMap<String, Integer>();
 	@Test
 	public void test() {
@@ -20,9 +21,10 @@ public class TestStringCache {
 		assertEquals(expected1, sCache.checkOccurrence(input1));
 		// re-using map and set
 		expectedMap.put("world!", 6);
-		System.out.println(expectedMap);
 		assertEquals(expected2, sCache.checkOccurrence(input2));
+		expectedMap.put("hello world!", 9);
+		assertEquals(expected3, sCache.checkOccurrence(input3));
+		assertEquals(expected1, sCache.checkOccurrence(input4));
+		System.out.println(sCache.cacheMap);
 	}
 }
-
-
