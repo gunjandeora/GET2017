e6807d3d6a8e74d0aaefad6ad956bd7e1931639e
gunjandeora <gunjan.deora@metacube.com>
2017-08-31 13:12:38 +0530

    Update ProductsConstants.java

--- DAO/ProductsConstants.java
+++ DAO/ProductsConstants.java
@@ -1,6 +1,5 @@
 package DAO;
 public class ProductsConstants {
-	public static final String PRODUCT_DETAILS_FILE_NAME="ProductDetails.txt";
-	public static final String PRODUCT_DETAILS_FILE_PATH="D://Training 2017/DesignPattern1/srcProductDetails.txt";
-	
+	public static final String productDetailsFileName="ProductDetails.txt";
+	public static final String productDetailsFilePath="D://Training 2017/DesignPattern1/srcProductDetails.txt";
 }
