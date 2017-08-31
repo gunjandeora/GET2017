a522416bb1918c770d7521df33a613d60a1b3521
gunjandeora <gunjan.deora@metacube.com>
2017-08-31 16:08:13 +0530

    Update LeftOccuranceTest.java

--- DS7/LeftOccuranceTest.java
+++ DS7/LeftOccuranceTest.java
@@ -5,10 +5,11 @@ public class LeftOccuranceTest {
 	@Test
 	public void leftOccuranceTest() {
 		BinarySearch test = new BinarySearch();
-		int array[] = new int[]{55,56,57,58,59,60};
+		int array[] = new int[]{55,56,57,58,59,60},array2[] = new int[1];
 		int expected1 = 2, 
 			expected2 = -1;	
 		assertEquals(expected1, test.startSearch(array, 57));			//if value is present in array
 		assertEquals(expected2, test.startSearch(array, 65));			//false test case
+		assertEquals(expected2, test.startSearch(array2, 57));
 	}
 }
