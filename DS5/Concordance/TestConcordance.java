f202e1d5704059ef353d0c22b1196b57f997427f
gunjandeora <gunjan.deora@metacube.com>
2017-08-31 17:02:36 +0530

    Update TestConcordance.java

--- DS5/Concordance/TestConcordance.java
+++ DS5/Concordance/TestConcordance.java
@@ -6,19 +6,24 @@ import org.junit.Test;
 import DS2.CounsellingSystem.MyArrayList;
 public class TestConcordance {
 	Concordance cor = new Concordance();
-	String testString = "hello world!";
-	Integer expected1 = 4,
-		expected2 = 9,
-		expected3 = 11;
+	
 	Map<Character, MyLinkList> map = new HashMap<Character, MyLinkList>();
 	@Test
 	public void testStringConcordance() {
+		String testString1 = "hello world!", 
+			testString2 = " ";
+		Integer expected1 = 4,
+			expected2 = 9,
+			expected3 = 11,
+			expected4 = 0;
 		MyLinkList<Integer> testList = new MyLinkList<Integer>();
-		cor.concordanceCharacter(testString);
+		cor.concordanceCharacter(testString1);
+		cor.concordanceCharacter(testString2);
 //		testList = cor.map.get('o');
 //		System.out.println("%%%%%%%" + testList.getAtLoc(1));
-		assertEquals(expected1, cor.map.get('o').getAtLoc(1));				//location of 'o' stored in MyLinkedList
-		assertEquals(expected2, cor.map.get('l').getAtLoc(3));				//location of third 'l' in MyLinkedList
-		assertEquals(expected3, cor.map.get('!').getAtLoc(1));				//location of '!' stored in MyLinkedList
+		assertEquals(expected1, cor.map.get('o').getAtLoc(1));							//location of 'o' stored in MyLinkedList
+		assertEquals(expected2, cor.map.get('l').getAtLoc(3));							//location of second 'l' in MyLinkedList
+		assertEquals(expected3, cor.map.get('!').getAtLoc(1));							//location of '!' in MyLinkedList 
+		assertEquals(expected4, cor.map.get(' ').getAtLoc(1));
 	}
 }
