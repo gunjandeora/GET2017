637c37cec80e27cf768d36dce637ee70ae6f5dd0
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 16:45:58 +0530

    Update DesignPattern2.java

--- DesignPattern2.java
+++ DesignPattern2.java
@@ -1,8 +1,8 @@
+import java.io.IOException;
 import Facade.Controller;
-
 //class having public static void main
 public class DesignPattern2 {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		Controller controller = new Controller();
 	}
 }
