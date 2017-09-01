0ff82ac69cf901c9472a480b57e663e774480678
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 13:13:11 +0530

    Update MyNode.java

--- DS8/TreeSort/MyNode.java
+++ DS8/TreeSort/MyNode.java
@@ -1,31 +1,24 @@
 package DS8.TreeSort;
 public class MyNode<T> {
-
 	MyNode<T> left, right;
 	T data;
-
 	MyNode(T data) {
 		this.data = data;
 		left = null;
 		right = null;
 	}
-
 	T getData() {
 		return data;
 	}
-
 	void setLeft(MyNode<T> node) {
 		this.left = node;
 	}
-
 	void setRight(MyNode<T> node) {
 		this.right = node;
 	}
-
 	MyNode<T> getLeft() {
 		return left;
 	}
-
 	MyNode<T> getRight() {
 		return right;
 	}
