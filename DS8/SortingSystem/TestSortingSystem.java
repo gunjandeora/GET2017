e73216fd6a647afc64f8724f25bb11f84130f47e
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 14:16:00 +0530

    Update TestSortingSystem.java

--- DS8/SortingSystem/TestSortingSystem.java
+++ DS8/SortingSystem/TestSortingSystem.java
@@ -1,49 +1,49 @@
 package DS8.SortingSystem;
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
-public class TestSortingSystem {
-
+public class SortingSystemTest {	
+	// setting input array for testing
+	public int[] getInputArray(){
+		return new int[]{1,111,543,7};
+	}
+	//setting expected array for testing
+	public int[] getExpectedArray(){
+		return new int[]{1,7,111,543};
+	}
+	//testing bubble sorting
 	@Test
 	public void bubbleTest() {
-		int array[] = new int[]{1,111,543,7};
-		int expected[] = new int[]{1,7,111,543};
-		
+		int  array2[] = null, expected2[] = null;	
 		BubbleSort obBubble = new BubbleSort();
-		int actual[] = obBubble.bubbleSort(array);
-		assertArrayEquals(expected, actual);
+		int actual[] = obBubble.bubbleSort(getInputArray());
+		assertArrayEquals(getExpectedArray(), actual);
+		assertArrayEquals(expected2, obBubble.bubbleSort(array2));
 	}
-	
+	// testing counting sort
 	@Test
 	public void countingTest() {
-		int array[] = new int[]{1,111,543,7};
-		int expected[] = new int[]{1,7,111,543};
-		
+		int  array2[] = null, expected2[] = null;	
 		CountingSort cSort = new CountingSort();
-		int actual[] = cSort.countingSort(array, 543);
-		assertArrayEquals(expected, actual);
+		int actual[] = cSort.countingSort(getInputArray(), 543);
+		assertArrayEquals(getExpectedArray(), actual);
+		assertArrayEquals(expected2, array2);
 	}
-	
+	//testing quick sort
 	@Test
 	public void quickTest() {
-		int array[] = new int[]{1,111,543,7};
-		int expected[] = new int[]{1,7,111,543};
-		
 		QuickSort qSort = new QuickSort();
-		int actual[] = qSort.quickSortResult(array, 0, array.length - 1);
-		assertArrayEquals(expected, actual);
+		int  array2[] = null, expected2[] = null;	
+		int actual[] = qSort.startQuickSort(getInputArray());
+		assertArrayEquals(getExpectedArray(), actual);
+		assertArrayEquals(array2, expected2);
 	}
 	
 	@Test
 	public void radixTest() {
-		int array[] = new int[]{1,111,543,7};
-		int expected[] = new int[]{1,7,111,543};
-		
 		RadixSort rSort = new RadixSort();
-		int actual[] = rSort.radixSort(array, 3);
-		assertArrayEquals(expected, actual);
+		int actual[] = rSort.radixSort(getInputArray(), 3);
+		int  array2[] = null, expected2[] = null;	
+		assertArrayEquals(getExpectedArray(), actual);
+		assertArrayEquals(expected2, array2);
 	}
-
 }
