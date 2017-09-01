53285048fbbe55b599357b74f7aeb2fb11989399
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 16:43:56 +0530

    Update UserCart.java

--- model/UserCart.java
+++ model/UserCart.java
@@ -2,15 +2,15 @@ package model;
 //Class containing hash map having details of ordered product	
 import java.util.HashMap;
 public class UserCart {
-	private HashMap<Product,OrderedProduct> selectedProducts=new HashMap<Product,OrderedProduct>();
+	public HashMap<Integer,OrderedProduct> selectedProducts=new HashMap<Integer,OrderedProduct>();
 	/*
 	 * getters and setter for data members of the class. 
 	 */
-	public HashMap<Product, OrderedProduct> getSelectedProducts() {
+	public HashMap<Integer, OrderedProduct> getSelectedProducts() {
 		return selectedProducts;
 	}
 	public void setSelectedProducts(
-			HashMap<Product, OrderedProduct> selectedProducts) {
+			HashMap<Integer, OrderedProduct> selectedProducts) {
 		this.selectedProducts = selectedProducts;
 	}
 }
