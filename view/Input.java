519beb301467222b9ca4144ddf6dd46525ce3d6a
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 16:45:19 +0530

    Update Input.java

--- view/Input.java
+++ view/Input.java
@@ -1,35 +1,44 @@
 package view;
 import java.util.Scanner;
 public class Input {
-	private static Input inputInstance;
 	Scanner inputScanner = new Scanner(System.in);
-	private Input() {}
-	// getting instance of singleton class Input
-	public static Input getInstance() {
-		if(inputInstance== null){
-			inputInstance = new Input();
-			return inputInstance;
-		}
-		else{
-			return inputInstance;			
-		}
-	}
 	// to get choice of user for menu items shown to user
 	public String getChoice() {
+		String choice = "";
 		System.out.println("Enter your choice");
-		String choice = inputScanner.next();
+		try{
+			choice = inputScanner.next();
+		}
+		catch(Exception e){
+			System.out.println("Invalid choice");
+			return "";
+		}
 		return choice;
 	}
 	// to get product id from user
 	public String getProductID() {
+		String productID = "";
 		System.out.println("Enter Product ID: ");
-		String productID = inputScanner.next();
+		try{
+			productID = inputScanner.next();
+		}
+		catch(Exception e){
+			System.out.println("Invalid choice");
+			return "";
+		}
 		return productID;
 	}
 	// to get product quantity
 	public String getProductQuantity() {
+		String productQuantity = "";
 		System.out.println("Enter Product Quantity: ");
-		String productQuantity = inputScanner.next();
+		try{
+			productQuantity = inputScanner.next();
+		}
+		catch(Exception e){
+			System.out.println("Invalid choice");
+			return "";
+		}
 		return productQuantity;
 	}
 }
