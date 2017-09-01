7f70acf1bc2f10576eb0b847f20e73a153d8ab1d
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 14:15:12 +0530

    Update QuickSort.java

--- DS8/SortingSystem/QuickSort.java
+++ DS8/SortingSystem/QuickSort.java
@@ -1,32 +1,34 @@
 package DS8.SortingSystem;
-
 class QuickSort {
 	/*
 	 * sort array & return resulting sorted array arr-->array to be sorted
 	 * first-->starting index last-->end index
 	 */
+	public int[] startQuickSort(int arr[]){
+		if(arr==null){
+			System.out.println("invalid input");
+			return null;
+		}
+		return quickSortResult(arr, 0, arr.length-1);
+	}
 	public int[] quickSortResult(int arr[], int first, int last) {
 		if (arr.length == 0) {
 			return null;
 		} else {
 			if (first < last) {
 				int partitionIndex = partitionArray(arr, first, last); // partitioning of array
-
 				// Recursive sorting approch
 				quickSortResult(arr, first, partitionIndex - 1);
 				quickSortResult(arr, partitionIndex + 1, last);
 			}
-
 			return arr;
 		}
 	}
-
 	/*
 	 * partition array & return index arr-->array to be sorted first-->starting
 	 * index last-->end index
 	 */
 	public int partitionArray(int arr[], int first, int last) {
-
 		int pivot = arr[last]; // pivot element
 		int index = first - 1;
 		for (int row = first; row <= last - 1; row++) {
@@ -37,12 +39,10 @@ class QuickSort {
 				arr[row] = temp;
 			}
 		}
-
 		// swapping of pivot element
 		int temp = arr[index + 1];
 		arr[index + 1] = arr[last];
 		arr[last] = temp;
-
 		return index + 1;
 	}
 }
