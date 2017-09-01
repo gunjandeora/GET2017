9eaa2f2e4f70e232a7e0cf7862425c0c66da64a1
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 14:15:29 +0530

    Update RadixSort.java

--- DS8/SortingSystem/RadixSort.java
+++ DS8/SortingSystem/RadixSort.java
@@ -1,28 +1,20 @@
 package DS8.SortingSystem;
-
-/**
+/*
  * radixsort user radix to sort array element using modulus operation and also
  * arrange in sorted order
  */
 public class RadixSort {
-/**
+/*
  * it sort the given array using radix
- * @param array
- * @param digitCount -> digit in maximum element in array
- * @return resultend array
  */
 	public int[] radixSort(int array[], int digitCount) {
-
 		int tempArr[] = array;
 		MyQueue queue[] = new MyQueue[10];
 		for (int index = 0; index < 10; index++) {
 			queue[index] = new MyQueue();
 		}
-
-		
 		for (int rowIndex = 0; rowIndex < digitCount; rowIndex++) {
 			for (int index = 0; index < array.length; index++) {
-				
 				queue[(tempArr[index] / power(10, rowIndex)) % 10].enqueue(tempArr[index]);  //logic to compute position via modulus operation using array Element & enqueue it
 			}
 			int count = 0;
@@ -31,13 +23,9 @@ public class RadixSort {
 					tempArr[count++] = queue[index].dequeue(); //dequeue and store it into tempArray
 				}
 			}
-
 		}
-
 		return tempArr;
-
 	}
-
 	//method to compute power
 	public int power(int number1, int number2) {
 		int power = 1;
