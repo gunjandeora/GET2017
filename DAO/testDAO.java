1eb9ccc5cf3deca7bcc612b5e82d62ee37576fd0
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 16:41:42 +0530

    Update testDAO.java

--- DAO/testDAO.java
+++ DAO/testDAO.java
@@ -1,17 +1,12 @@
 package DAO;
-
 import static org.junit.Assert.*;
-
 import java.io.IOException;
-
 import org.junit.Test;
-
-public class testDAO {
+public class ProductDetailsInMemoryTest {
 	ProductDetailsInMemory pd = new ProductDetailsInMemory();
 	String path = "D://Training 2017/DesignPattern1/src/ProductDetails.txt";
 	@Test
-	public void test() throws IOException {
-		pd.readProductDetails();
+	public void readProductDetails_NoInput_ListOfProduct() throws IOException {
+		assertEquals(1001, pd.readProductDetails().get(0).getProductID());
 	}
-
 }
