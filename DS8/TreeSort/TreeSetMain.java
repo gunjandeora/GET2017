eeb0bcc25b85affa4fd977e373a75c6ae321ee8f
gunjandeora <gunjan.deora@metacube.com>
2017-09-01 13:17:59 +0530

    Update TreeSetMain.java

--- DS8/TreeSort/TreeSetMain.java
+++ DS8/TreeSort/TreeSetMain.java
@@ -1,29 +1,36 @@
 package DS8.TreeSort;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import DS2.Queue.MyArrayList;
 public class TreeSetMain {
-
+	Scanner sc = new Scanner(System.in);
+	MyBinarySearchTree bt = new MyBinarySearchTree();
 	public static void main(String[] args) {
-		Student stud1 = new Student(101, "Abhishek");
-		Student stud2 = new Student(102, "Rohit");
-		Student stud3 = new Student(103, "Akash");
-		Student stud4 = new Student(104, "Maynk");
-		Student stud5 = new Student(105, "Shobhit");
-
-		MyBinarySearchTree bt = new MyBinarySearchTree();
-		bt.insertNode(stud2);
-		bt.insertNode(stud1);
-		bt.insertNode(stud3);
-		bt.insertNode(stud4);
-		bt.insertNode(stud5);
-		
-		System.out.println(bt.size(bt.getRoot()));
-		
-		List<Integer> sortedList = new ArrayList<Integer>(); 
-		sortedList= bt.inOrder(bt.getRoot() , sortedList);
-		
-		System.out.println(sortedList);
-
+		TreeSetMain treeSet = new TreeSetMain();
+		treeSet.takeInput();		
 	}
-
+	public void takeInput(){
+		String name, choice="y";
+		int roll;
+		while(choice.equals("y") || choice.equals("Y")){
+			System.out.println("Enter Name of Student ");
+			name = sc.nextLine();
+			System.out.println("Enter Roll Number of Student");
+			try{									//exception handling for wrong roll number
+				roll = sc.nextInt();
+			}
+			catch(Exception e){
+				System.out.println("Enter valid roll number");
+				continue;
+			}
+			bt.insertNode(new Student(roll, name));	
+			System.out.println("Do you wish to add more students?? Enter y or n");
+			choice = sc.nextLine();
+		}
+		System.out.println(" Number of Students: "+ bt.size(bt.getRoot()));		
+		MyArrayList sortedList = new MyArrayList();
+		sortedList= bt.inOrder(bt.getRoot() , sortedList);			//retrieving sorted list of roll numbers
+		System.out.println(sortedList);	
+	}
 }
