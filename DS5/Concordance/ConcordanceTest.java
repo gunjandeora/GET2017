bf5225e734d97b4fdd779e5fe6a4e9ce2d23c79c
gunjandeora <gunjan.deora@metacube.com>
2017-09-25 16:32:17 +0530

    Update ConcordanceTest.java

--- DS5/Concordance/ConcordanceTest.java
+++ DS5/Concordance/ConcordanceTest.java
@@ -5,21 +5,19 @@ import java.util.Map;
 import org.junit.Test;
 import DS2.CounsellingSystem.MyArrayList;
 public class ConcordanceTest {
+	Concordance cor = new Concordance();
+	Map<Character, MyLinkList> map = new HashMap<Character, MyLinkList>();
 	@Test
-	public void concordanceCharacter_NoInputValue_intValue() {
-		Concordance cor = new Concordance();
-		Map<Character, MyLinkList> map = new HashMap<Character, MyLinkList>();
+	public void concordanceCharacter_String_Integer() {
 		String testString1 = "hello world!", 
-			testString2 = " ";
+				testString2 = " ";
 		Integer expected1 = 4,
-			expected2 = 9,
-			expected3 = 11,
-			expected4 = 0;
+				expected2 = 9,
+				expected3 = 11,
+				expected4 = 0;
 		MyLinkList<Integer> testList = new MyLinkList<Integer>();
 		cor.concordanceCharacter(testString1);
 		cor.concordanceCharacter(testString2);
-//		testList = cor.map.get('o');
-//		System.out.println("%%%%%%%" + testList.getAtLoc(1));
 		assertEquals(expected1, cor.map.get('o').getAtLoc(1));							//location of 'o' stored in MyLinkedList
 		assertEquals(expected2, cor.map.get('l').getAtLoc(3));							//location of second 'l' in MyLinkedList
 		assertEquals(expected3, cor.map.get('!').getAtLoc(1));							//location of '!' in MyLinkedList 
