ecb0e833782416ff612abe879c44b3a76ebbab1a
gunjandeora <gunjan.deora@metacube.com>
2017-09-26 15:14:48 +0530

    Update ConcordanceTest.java

--- DS5/Concordance/ConcordanceTest.java
+++ DS5/Concordance/ConcordanceTest.java
@@ -1,14 +1,17 @@
 package DS5.Concordance;
 import static org.junit.Assert.*;
+
 import java.util.HashMap;
 import java.util.Map;
+
 import org.junit.Test;
+
 import DS2.CounsellingSystem.MyArrayList;
 public class ConcordanceTest {
 	Concordance cor = new Concordance();
 	Map<Character, MyLinkList> map = new HashMap<Character, MyLinkList>();
 	@Test
-	public void concordanceCharacter_String_Integer() {
+	public void concordanceCharacter_String_positionsOfCharacters() {
 		String testString1 = "hello world!", 
 				testString2 = " ";
 		Integer expected1 = 4,
@@ -22,5 +25,11 @@ public class ConcordanceTest {
 		assertEquals(expected2, cor.map.get('l').getAtLoc(3));							//location of second 'l' in MyLinkedList
 		assertEquals(expected3, cor.map.get('!').getAtLoc(1));							//location of '!' in MyLinkedList 
 		assertEquals(expected4, cor.map.get(' ').getAtLoc(1));
+	}
+	
+	@Test
+	public void concordanceCharacter_nullString_null() {
+		String	testString1 = null;
+		assertEquals(null, cor.concordanceCharacter(testString1));
 	}
 }
