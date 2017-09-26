7ae738e6d03a7a51e1d42896b0109fddf2fbf743
gunjandeora <gunjan.deora@metacube.com>
2017-09-26 15:14:16 +0530

    Update MyLinkedListTest.java

--- DS5/Concordance/MyLinkedListTest.java
+++ DS5/Concordance/MyLinkedListTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class MyLinkedListTest {
 	MyLinkList<Integer> list = new MyLinkList<Integer>();
 	@Test
-	public void getAtLoc_location_valueAtLocation() {
+	public void getAtLoc_validLocation_valueAtLocation() {
 		Integer expected1 = 1,
 				expected2 = 3, 
 				expected3 = 5;
@@ -15,7 +15,7 @@ public class MyLinkedListTest {
 		assertEquals(null, list.getAtLoc(23));
 	}
 	@Test
-	public void getAtLoc_location_null() {
+	public void getAtLoc_invalidLocation_null() {
 		list.insert(1);
 		list.insert(3);
 		list.insert(5);
@@ -33,19 +33,30 @@ public class MyLinkedListTest {
 	}
 	
 	@Test
-	public void deleteAtLoc_location_boolean(){
+	public void deleteAtLoc_validLocation_boolean(){
 		Integer expected = 14;		
 		insertValues();
 		assertEquals(true, list.deleteAtLoc(3));
 		assertEquals(expected, list.getAtLoc(3));		
 	}
 	@Test
-	public void deleteByValue_value_boolean(){
+	public void deleteAtLoc_invalidLocation_boolean(){
+		Integer expected = 14;		
+		insertValues();
+		assertEquals(false, list.deleteAtLoc(333));		
+	}
+	@Test
+	public void deleteByValue_valuePresent_boolean(){
 		Integer expected = 13;
 		insertValues();
 		assertEquals(true, list.deleteByValue(12));		
 		System.out.println("value" + list.getAtLoc(2));
 		assertEquals(expected, list.getAtLoc(2));	
+		assertEquals(false, list.deleteByValue(122));
+	}
+	@Test
+	public void deleteByValue_valueNotPresent_boolean(){
+		insertValues();	
 		assertEquals(false, list.deleteByValue(122));
 	}
 	public void insertValues(){
