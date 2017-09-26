a5abc13d0f2ad5b748df6606d1ecdb3fc9e48709
gunjandeora <gunjan.deora@metacube.com>
2017-09-26 15:16:49 +0530

    Update MyQueueTest.java

--- DS5/QueueImplementation/MyQueueTest.java
+++ DS5/QueueImplementation/MyQueueTest.java
@@ -1,5 +1,6 @@
 package DS5.QueueImplementation;
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 public class MyQueueTest {
 	MyQueue myQueue = new MyQueue();	
@@ -9,7 +10,7 @@ public class MyQueueTest {
 		assertEquals(true, myQueue.enqueue(5));
 	}
 	@Test
-	public void dequeue_null_Integer(){
+	public void dequeue_null_valueDeleted(){
 		myQueue.enqueue(22);
 		myQueue.enqueue(25);
 		myQueue.enqueue(35);
@@ -19,7 +20,7 @@ public class MyQueueTest {
 		assertEquals(35, myQueue.dequeue());
 	}
 	@Test
-	public void getFront_null_Integer() {
+	public void getFront_null_valueOfFrontNode() {
 		myQueue.enqueue(35);
 		myQueue.enqueue(45);
 		myQueue.enqueue(55);
@@ -28,7 +29,7 @@ public class MyQueueTest {
 		assertEquals(35, myQueue.getFront());
 	}
 	@Test 
-	public void getSize_null_Integer(){
+	public void getSize_null_sixeOfMyQueue(){
 		myQueue.enqueue(55);
 		myQueue.enqueue(65);
 		assertEquals(2, myQueue.size);
