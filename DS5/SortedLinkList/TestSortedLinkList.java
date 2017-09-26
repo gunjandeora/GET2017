641116224d83557df97d4fbd7df098ecbd69a0f8
gunjandeora <gunjan.deora@metacube.com>
2017-09-26 15:28:20 +0530

    Update TestSortedLinkList.java

--- DS5/SortedLinkList/TestSortedLinkList.java
+++ DS5/SortedLinkList/TestSortedLinkList.java
@@ -1,12 +1,11 @@
 package DS5.SortedLinkList;
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 public class TestSortedLinkList {
 	static MyLinkList<Integer> testList = new MyLinkList<Integer>();
 	Node<Integer> head ;
 	@Test
-	public void testEmptyList(){
+	public void EmptyListTest_null_boolean(){
 		assertEquals(true, testList.isEmpty());
 		Node<Integer> head = testList.insert(testList.getHead(), 2);
 		testList.setHead(head);
@@ -17,7 +16,7 @@ public class TestSortedLinkList {
 		assertEquals(false, testList.isEmpty());
 	}
 	@Test
-	public void testSize() {
+	public void getSize_null_sizeOfLinkList() {
 		Node<Integer> head = testList.insert(testList.getHead(), 2);
 		testList.setHead(head);
 		testList.insertNode(4);
@@ -26,5 +25,8 @@ public class TestSortedLinkList {
 		testList.insertNode(10);
 		System.out.println(testList.getSize());
 		assertEquals(5, testList.size);
+	}
+	private void insertData(){
+		
 	}
 }
