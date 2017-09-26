d4ef5aae817f9f12e64943ec0b21f148b685e369
gunjandeora <gunjan.deora@metacube.com>
2017-09-26 15:12:56 +0530

    Update BinarySearchTest.java

--- DS7/BinarySearchTest.java
+++ DS7/BinarySearchTest.java
@@ -1,15 +1,22 @@
 package DS7;
 import static org.junit.Assert.*;
 import org.junit.Test;
-public class BinarySearchTest {
+public class LeftOccuranceTest {
+	BinarySearch test = new BinarySearch();
+	int array1[] = new int[]{55,56,57,58,59,60},
+		array2[] = null;
+	int expected1 = 2, 
+		expected2 = -1;	
 	@Test
-	public void startSearch_array_intValue () {
-		BinarySearch test = new BinarySearch();
-		int array[] = new int[]{55,56,57,58,59,60},array2[] = new int[1];
-		int expected1 = 2, 
-			expected2 = -1;	
-		assertEquals(expected1, test.startSearch(array, 57));			//if value is present in array
-		assertEquals(expected2, test.startSearch(array, 65));			//false test case
-		assertEquals(expected2, test.startSearch(array2, 57));
+	public void leftOccuranceTest_intArrayNvaluePresent_positionofValue() {
+		assertEquals(expected1, test.startSearch(array1, 57));			//if value is present in array
+	}
+	@Test
+	public void leftOccuranceTest_intArrayNvalueNotPresent_positionofValue() {
+		assertEquals(expected2, test.startSearch(array1, 65));			//false test case, if value not present
+	}
+	@Test
+	public void leftOccuranceTest_nullArray_negativeValue() {
+		assertEquals(expected2, test.startSearch(array2, 57));			//returns -1 i array is null
 	}
 }
