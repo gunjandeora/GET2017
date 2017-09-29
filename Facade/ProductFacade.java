279625f93ae94c0c5f6820adf599bea1007e5474
gunjandeora <gunjan.deora@metacube.com>
2017-09-29 12:46:21 +0530

    Update ProductFacade.java

--- Facade/ProductFacade.java
+++ Facade/ProductFacade.java
@@ -1,14 +1,11 @@
 package Facade;
 import java.util.HashMap;
-
 import DAO.ProductDetailsDao;
 import DAO.ProductsConstants;
 import Facade.ProductFacade;
 import Model.ProductDetails;
-import View.GetInput;
 //Facade class to perform business logics and to validate input from the user
 public class ProductFacade {
-	String filePath;
 	private static ProductFacade productFacadeObject;
 	private HashMap<Integer,ProductDetails> productDetailsList=new HashMap<Integer,ProductDetails>();
 	ProductDetailsDao productDetailsDaoObject = ProductDetailsDao.getInstance();
@@ -28,11 +25,6 @@ public class ProductFacade {
 	public void startProject(){
 		ProductDetailsDao productDetailsDaoObject = ProductDetailsDao.getInstance();				// Initializing DAO object
 		retrieveValues();
-		GetInput input = new GetInput();
-		String tempProductId = input.getProductIDFromUser();
-		input.updateFile(tempProductId);														//updating information for any ID
-		input.addProduct(); 																	//adding value for any product in the list	
-		
 	}
 	
 	// getter and setter of Hash map of productDetailsList containing key as productID
@@ -44,12 +36,12 @@ public class ProductFacade {
 	}
        //method to retrieve values from the files using dao class
 	public void retrieveValues(){
-	productDetailsDaoObject.setFilePath(ProductsConstants.productDetailsFileName);		//Initializing file to retrieve data
+	productDetailsDaoObject.setFilePath(ProductsConstants.productdetailsFilePath);		//Initializing file to retrieve data
 	productDetailsList=productDetailsDaoObject.readProductDetails();						//saving data in List
 	}
-   /*
-    * method to update the products in files and in hashmaps
-    */
+  	 /*
+    	  * method to update the products in files and in hashmaps
+   	  */
 	public void updateDetails(ProductDetails productDetailsObject){
 		int productCode=productDetailsObject.getProductCode();
 		productDetailsList.put(productCode, productDetailsObject);
